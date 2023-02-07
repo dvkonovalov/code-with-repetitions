63d4e0fa29a145733411f20016fe0cedc8778250
Daniil Konovalov <dvkonovalov@miem.hse.ru>
2023-02-07 10:01:41 +0300

    second part

--- main.py
+++ main.py
@@ -15,3 +15,24 @@ def repeat_decoding(y, s):
         fragment = Counter(y[i * s:(i + 1) * s])
         answer += fragment.most_common(1)[0][0]
     return answer
+
+
+def binary_channel(p, y):
+    answer = ''
+    summa = 0
+    for i in y:
+        summa += p
+        if (summa-1.0<0.001):
+            answer += i
+        else:
+            summa -= 1
+            answer += str((int(i) + 1) % 2)
+    return answer
+
+
+c = repeat_encoding('10101', 5)
+print(c)
+c = binary_channel(0.34, c)
+print(c)
+c = repeat_decoding(c, 5)
+print(c)

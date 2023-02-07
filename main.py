53641682826e7125f94ba30baace5d10a8ff7dc1
Daniil Konovalov <dvkonovalov@miem.hse.ru>
2023-02-07 11:43:29 +0300

    final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
+from random import randint
 from collections import Counter
+import matplotlib.pyplot as plt
 
 
 def repeat_encoding(x, s):
@@ -21,12 +22,79 @@ def repeat_decoding(y, s):
 def binary_channel(p, y):
     errors = []
     for i in range(round(p*len(y))):
-        place = random.randint(0,len(y)-1)
+        place = randint(0,len(y)-1)
         while (place in errors):
-            place = random.randint(0, len(y) - 1)
+            place = randint(0, len(y) - 1)
         errors.append(place)
         y = y[:place] + str((int(y[place])+1)%2) + y[place+1:]
     return y
+
+
+def first_grafic(count):
+    x = []
+    y = []
+    stroka = ''
+    for i in range(count):
+        stroka += str(randint(0,1))
+        send_stroka = repeat_decoding(binary_channel(0.3, repeat_encoding(stroka, 3)), 3)
+        summa = 0
+        for j in range(i+1):
+            if (stroka[j]!=send_stroka[j]):
+                summa += 1
+        if (summa!=0):
+            y.append(summa)
+            x.append(i + 1)
+    plt.plot(x, y)
+    plt.title('График 1')
+    plt.ylabel('Количество ошибок')
+    plt.xlabel('Количество символов в строке')
+    plt.show()
+
+
+def second_grafic(shag):
+    p = 0
+    x = []
+    y = []
+    stroka = ''
+    for i in range(100):
+        stroka += str(randint(0,1))
+    while (p<1.0):
+        send_stroka = repeat_decoding(binary_channel(p, repeat_encoding(stroka, 5)), 5)
+        summa = 0
+        for i in range(100):
+            if (stroka[i]!=send_stroka[i]):
+                summa += 1
+        if (summa!=0):
+            y.append(summa)
+            x.append(p)
+        p += shag
+    plt.plot(x, y)
+    plt.title('График 2')
+    plt.ylabel('Количество ошибок')
+    plt.xlabel('Вероятность ошибки')
+    plt.show()
+
+def third_grafic(maximum):
+    x = []
+    y = []
+    stroka = ''
+    for i in range(100):
+        stroka += str(randint(0,1))
+    for s in range(1, maximum+1):
+        send_stroka = repeat_decoding(binary_channel(0.3, repeat_encoding(stroka, s)), s)
+        summa = 0
+        for i in range(100):
+            if (stroka[i]!=send_stroka[i]):
+                summa += 1
+        if (summa!=0):
+            y.append(summa)
+            x.append(s)
+    plt.plot(x, y)
+    plt.title('График 3')
+    plt.ylabel('Количество ошибок')
+    plt.xlabel('Количество повторений')
+    plt.show()
+
 
 
 c = repeat_encoding('10110', 5)
@@ -35,3 +103,6 @@ c = binary_channel(0.05, c)
 print(c)
 c = repeat_decoding(c, 5)
 print(c)
+first_grafic(100)
+second_grafic(0.05)
+third_grafic(100)
